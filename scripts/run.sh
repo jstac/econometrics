4aa608580b04f073b434be2e50c1df62b9970774
Matthew McKay <mamckay@gmail.com>
2015-08-13 13:47:30 -0400

    Adjustment to pull latest tmpnb

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker pull jupyter/minimal
+docker pull jupyter/tmpnb
 docker pull sanguineturtle/econometrics
 export TOKEN=$( head -c 30 /dev/urandom | xxd -p )
 docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN --name=proxy jupyter/configurable-http-proxy --default-target http://127.0.0.1:9999
