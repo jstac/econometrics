7c188dc598a8fa7358d063d79f74af8359580158
Matt McKay <mamckay@gmail.com>
2015-11-03 14:26:03 -0500

    Update to gravity model example including data and basic notebook

--- gravity/data/construct_data.py
+++ gravity/data/construct_data.py
@@ -24,28 +24,13 @@ Notes
 1. 	The data source is currently available for 1962 to 2013.
 	Due to the large size of the dataset it needs to be downloaded
 	from the source and uncompressed.
-"""
 
-import pandas as pd
-import dask.dataframe as dd
+2. 	Using dask
 
-#-Trade Data-#
-trade = dd.read_csv("year_origin_destination_sitc_rev2.tsv", sep="\t", header=0)
-trade = trade.rename(columns={ "year" : 'y',
-							"origin" : 'i',
-							"destination" : 'j',
-							"sitc_rev2" : 'p',
-							"export_val" : 'ex',
-							"import_val" : 'im',
-							})
-#-Year: 2013-#
-trade = trade[trade.y == 2013]
-trade = trade[['y','i','j','ex','im']].groupby(["y","i","j"]).sum()
-trade = trade.compute()
-#-Correct Country Codes-#
-trade = trade.reset_index()
-trade.i = trade.i.apply(lambda x: x.upper())
-trade.j = trade.j.apply(lambda x: x.upper())
+	#Using Chunks
+	# iter_csv = pd.read_csv('year_origin_destination_sitc_rev2.tsv', sep='\t', iterator=True, chunksize=100000)
+	# df = pd.concat([chunk[chunk['year'] == 2013] for chunk in iter_csv])
+	# df = df[['year','origin','destination','export_val','import_val']].groupby(["year","origin","destination"]).sum()
 
 	#-Equivalent Pandas Code takes ~18GB RAM due to pandas copies
 	# trade = pd.read_csv('year_origin_destination_sitc_rev2.tsv', sep='\t')
@@ -59,6 +44,85 @@ trade.j = trade.j.apply(lambda x: x.upper())
 	# #-Year: 2013-#
 	# data2 = trade[trade.y == 2013]
 	# data2 = data[['y','i','j','ex','im']].groupby(["y","i","j"]).sum()
+"""
+
+import pandas as pd
+import dask.dataframe as dd
+
+#------------#
+#-Trade Data-#
+#------------#
+# trade = dd.read_csv("year_origin_destination_sitc_rev2.tsv", sep="\t", header=0)
+#-Year: 2013-#
+# trade = trade[trade.year == 2013]
+# trade = trade[['year','origin','destination','export_val','import_val']].groupby(["year","origin","destination"]).sum()
+# trade = trade.compute()
+
+#-Chunks-#
+#-Year: 2013-#
+iter_csv = pd.read_csv('year_origin_destination_sitc_rev2.tsv', sep='\t', iterator=True, chunksize=100000)
+trade = pd.concat([chunk[chunk['year'] == 2013] for chunk in iter_csv])
+trade = trade[['year','origin','destination','export_val','import_val']].groupby(["year","origin","destination"]).sum()
+#-Correct Country Codes-#
+trade = trade.reset_index()
+trade.origin = trade.origin.apply(lambda x: x.upper())
+trade.destination = trade.destination.apply(lambda x: x.upper())
+
+#-Export and Import Data-#
+data = trade.copy()
+ex = data[["year","origin","destination","export_val"]].rename(columns={'export_val':'value','origin':'eiso3c','destination':'iiso3c'})
+im = data[["year","origin","destination","import_val"]].rename(columns={'import_val':'value','origin':'iiso3c','destination':'eiso3c'})
+
+#----------------#
+#-Attribute Data-#
+#----------------#
+
+#-Merge Importer Reported Data Together-#
+dataset = im.copy()
+
+#-Bilateral Relationships-#
+dist = pd.read_excel('dist_cepii.xls')
+bilat_attrs = ["iso_o","iso_d","contig","comlang_off","colony","dist","distcap","distw","distwces"]
+dataset = dataset.merge(dist[bilat_attrs], left_on=["eiso3c","iiso3c"], right_on=["iso_o","iso_d"], how="inner")
+for item in ["iso_o","iso_d"]:
+	del dataset[item]
+
+#-Country Attributes-#
+geo = pd.read_excel('geo_cepii.xls')
+cntry_attrs = ["iso3","landlocked"]
+dataset = dataset.merge(geo[cntry_attrs], left_on="eiso3c", right_on="iso3", how="inner")
+del dataset["iso3"]
+dataset.rename(columns={'landlocked':'ell'}, inplace=True)
+dataset = dataset.merge(geo[cntry_attrs], left_on="iiso3c", right_on="iso3", how="inner")
+del dataset["iso3"]
+dataset.rename(columns={'landlocked':'ill',}, inplace=True)
+
+#-WDI-#
+wdi = pd.read_csv('WDI_Data.csv')
+idx = ["Country Code","Indicator Code"]
+years = ["2013"]
+wdi = wdi[idx+years]
+wdi.columns = ["iso3c","code","value"]
+wdi = wdi.set_index(["iso3c","code"]).unstack("code")
+wdi.columns = wdi.columns.droplevel()
+# NY.GDP.MKTP.KD = GDP (constant 2005 US$)
+# NY.GDP.PCAP.KD = GDP per capita (constant 2005 US$)
+codes = ['NY.GDP.MKTP.KD','NY.GDP.PCAP.KD', 'SP.POP.TOTL']
+wdi_data = wdi[codes]
+wdi_data.columns = ['egdp', 'egdppc','epop']
+wdi_data = wdi_data.reset_index()
+dataset = dataset.merge(wdi_data, left_on="eiso3c", right_on="iso3c")
+del dataset['iso3c']
+wdi_data = wdi[codes]
+wdi_data.columns = ['igdp', 'igdppc','ipop']
+wdi_data = wdi_data.reset_index()
+dataset = dataset.merge(wdi_data, left_on="iiso3c", right_on="iso3c")
+del dataset['iso3c']
+
+#-Save Dataset-#
+dataset.to_csv("gravity_dataset_2013.csv", index=False)
+
+#--------- Notes ----------#
 
 # Note: This dataset contains exporter AND importer reports so should just use exports OR imports
 	# Compare Exporter and Importer Reports
@@ -69,24 +133,3 @@ trade.j = trade.j.apply(lambda x: x.upper())
 	# ex.rename(columns={'ex':'v'}, inplace=True)
 	# im.rename(columns={'im':'v'}, inplace=True)
 	# data = ex.append(im)
-data = trade.copy()
-ex = data[["y","i","j","ex"]].rename(columns={'ex':'value','i':'eiso3c','j':'iiso3c','y':'year'})
-im = data[["y","i","j","im"]].rename(columns={'im':'value','i':'iiso3c','j':'eiso3c','y':'year'})
-
-#-Get Attribute Data-#
-geo = pd.read_excel('geo_cepii.xls')
-dist = pd.read_excel('dist_cepii.xls')
-wdi = pd.read_csv('WDI_Data.csv')
-
-#-Merge Importer Reported Data Together-#
-dataset = im.copy()
-#-Bilateral Relationships-#
-# TODO
-#-Country Attributes-#
-cntry_attrs = ["iso3","landlocked"]
-dataset = dataset.merge(geo[cntry_attrs], left_on="eiso3c", right_on="iso3", how="left")
-del dataset["iso3"]
-dataset.rename(columns={'landlocked':'ell'}, inplace=True)
-dataset = dataset.merge(geo[cntry_attrs], left_on="iiso3c", right_on="iso3", how="left")
-del dataset["iso3"]
-dataset.rename(columns={'landlocked':'ill',}, inplace=True)
